b3c7544dc2d1876865420855b4e649d7be88c591
ayush-maddhesiya <118817485+ayush-maddhesiya@users.noreply.github.com>
2023-08-25 07:58:32 +0530

    Create java_first.java

---
+++ java_first.java
@@ -0,0 +1,26 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class HelloWorld {
+    public static void main(String[] args) {
+        //System.out.println("Hello, World!");
+        boolean bool = true;
+        int n = 452;
+        char ch = 'k';
+        long j = 1322;
+        float fol = 12132.33f;
+        double jo = 121312313.9091;
+        byte b = 120;
+        short sh = 90;
+        System.out.println("this is short " + sh);
+        System.out.println("this is byte "  + b);
+        System.out.println("this is double " + jo);
+        System.out.println("this is float " + fol);
+        System.out.println("this is  long " + j);
+        System.out.println("this is character " + ch);
+        System.out.println("this is bool " + bool);
+        System.out.println("this is interger " + n);
+        
+    
+    }
+}
